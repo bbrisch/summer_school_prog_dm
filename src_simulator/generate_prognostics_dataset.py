a4fd4e5fc9e6691c98311fa2d8b38cf07a1173e8
bbrisch <benjaminbritoschiele@gmail.com>
2026-05-28 13:54:31 +0200

    generate train, validation and test splits

--- src_simulator/generate_prognostics_dataset.py
+++ src_simulator/generate_prognostics_dataset.py
@@ -8,59 +8,72 @@ import matplotlib.pyplot as plt
 from simulator_class import SimulatorSimple
 
 
-# %% Load configurations for the simulator
-with open("sim_config.yml", "rb") as f:
-    sim_config = yaml.safe_load(f)
+def process_data(key, config):
 
+    # Memory for the simulated trajectories
+    processed_data = []
+    load_column = []
+    id_column = []
 
-# %% Generate simulations
+    # Loading profiles
+    loading_profiles = config["loading_profiles"]
 
-# Memory for the simulated trajectories
-processed_data = []
-load_column = []
-id_column = []
+    exp_id = 0
+    if key == "train":
+        n_simu = config["n_simu_train"]
+    elif key == "validation":
+        n_simu = config["n_simu_val"]
+    elif key == "test":
+        n_simu = config["n_simu_test"]
 
-# Loading profiles
-loading_profiles = sim_config["loading_profiles"]
+    for i, load in enumerate(loading_profiles):
 
-exp_id = 0
-for i, load in enumerate(loading_profiles):
+        # Initialize simulator
+        sim = SimulatorSimple(
+            n_simu,
+            config["v_cut"],
+            config["soc_0"],
+            load,
+            config["model_params"],
+        )
 
-    # Initialize simulator
-    sim = SimulatorSimple(
-        sim_config["n_simu"],
-        sim_config["v_cut"],
-        sim_config["soc_0"],
-        load,
-        sim_config["model_params"],
+        # Generate simulations
+        sim.simulate()
+
+        # Post process the simulations
+        for s in sim.v_memo.T:
+            s = s[
+                config["v_cut"] * 1.1 < s
+            ]  # Eliminate data that falls under the threshold
+            s = -1 * (s - s[0]) / s[0]  #
+
+            processed_data.append(s)
+            id_column.append(np.ones_like(s) * exp_id)
+            load_column.append(np.ones_like(s) * i)
+
+            exp_id += 1
+
+    # Generate a dataframe with the columns
+    cm_column = np.concatenate(processed_data, 0)  # column of condition monitoring data
+    load_type_column = np.concatenate(load_column, 0)
+    exp_id_column = np.concatenate(id_column, 0)
+
+    df = pd.DataFrame(
+        np.stack([cm_column, load_type_column, exp_id_column], 1),
+        columns=["hi", "load", "exp_id"],
     )
 
-    # Generate simulations
-    sim.simulate()
-
-    # Post process the simulations
-    for s in sim.v_memo.T:
-        s = s[
-            sim_config["v_cut"] * 1.1 < s
-        ]  # Eliminate data that falls under the threshold
-        s = -1 * (s - s[0]) / s[0]  #
-
-        processed_data.append(s)
-        id_column.append(np.ones_like(s) * exp_id)
-        load_column.append(np.ones_like(s) * i)
-
-        exp_id += 1
+    # Save dataframe as .parquet
+    df.to_parquet(os.path.join("data", f"{key}.parquet"))
 
 
-# %% Generate a dataframe with the columns
-cm_column = np.concatenate(processed_data, 0)  # column of condition monitoring data
-load_type_column = np.concatenate(load_column, 0)
-exp_id_column = np.concatenate(id_column, 0)
+if __name__ == "__main__":
 
-df = pd.DataFrame(
-    np.stack([cm_column, load_type_column, exp_id_column], 1),
-    columns=["hi", "load", "exp_id"],
-)
+    os.makedirs("data", exist_ok=True)
 
-# Save dataframe as .parquet
-df.to_parquet("prognostics_data.parquet")
+    # Load configurations for the simulator
+    with open("sim_config.yml", "rb") as f:
+        sim_config = yaml.safe_load(f)
+
+    for k in ["train", "validation", "test"]:
+        process_data(k, sim_config)
