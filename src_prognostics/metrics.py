21a2cfd021abbdb46b6f41adcc8006f2159cde51
bbrisch <benjaminbritoschiele@gmail.com>
2026-06-01 14:50:20 +0200

    add crps to the test metrics

--- src_prognostics/metrics.py
+++ src_prognostics/metrics.py
@@ -5,6 +5,14 @@ def mae(expected: np.array, actual_RUL: np.array):
     return abs(expected - actual_RUL).mean()
 
 
+def crps(prognostic_pdf: np.array, actual_RUL: np.array):
+    cdf = prognostic_pdf.cumsum(1)
+    l, s = cdf.shape
+    axis = np.arange(s).reshape(1, -1).repeat(l, 0)
+    heavyside = np.heaviside(axis - actual_RUL.reshape(-1, 1), 0)
+    return ((cdf - heavyside) ** 2).sum(1).mean()
+
+
 def pinaw(lower_bound: np.array, upper_bound: np.array):
     return (upper_bound - lower_bound).mean()
 
